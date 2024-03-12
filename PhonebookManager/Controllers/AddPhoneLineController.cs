c970df0bca026794437cd942669e7c9810701611
andrei.nicolae <andrei.nicolae@mmrmakita.eu>
2024-03-12 11:41:27 +0200

    quick add page

---
+++ PhonebookManager/Controllers/AddPhoneLineController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PhonebookManager.Controllers
+{
+    public class AddPhoneLineController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
